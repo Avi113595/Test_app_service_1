a5fb5375cf796b30ad36ac771ceed96f13952e81
AVINASH KUMAR <avinashkumar149@outlook.com>
2025-05-25 22:40:11 +0530

    Add project files.

---
+++ Test_app_service/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Test_app_service.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
